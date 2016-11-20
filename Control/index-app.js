6a7ba928d5d8952f89086873f82919b28bd388ce
Alex <ajshort2010@hotmail.com>
2016-11-20 12:55:51 -0800

    ground work started for the first page, index has been scaffolded and organized.

---
+++ Control/index-app.js
@@ -0,0 +1,9 @@
+'use strict'
+
+/*
+So to begin I need to work out several aspects. I will start with a character placeholder and work out the stat system and then begin work on the user character creator system.
+After that comes the battle system.
+Then how the user will travel. Do I make this all one page? Ideally I would want it that way but I currently don't have the skills to make that happen.  What to do... That will be a bridge I cross when I come to it. Either way user travel should be simple and intuitive. Buttons usually fill those requirements.
+
+Limit global variables by calling them in local scopes, plan out simple and efficient functions.
+*/
